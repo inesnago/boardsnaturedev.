ec57985ff5aa418f3fbe8caae40f422eaa433641
inesnago <imsnago@gmail.com>
2020-10-02 20:26:36 +0000

    starting dakr/light mode

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#show-ul").mouseenter(function(){
         $(".sub-nav-ul").show("slow");
     });
-    
+
     $("#show-ul").mouseleave(function(){
         $(".sub-nav-ul").hide("slow");
     });
@@ -15,4 +15,26 @@ $(document).ready(function(){
         $(this).removeClass("shade-dark");
     });
 
+    $("#dark").click(function(){
+        $("body").addClass("body-dark");
+        $("a").addClass("a-dark");
+    })
+
+    $("#light").click(function(){
+        $("body").removeClass("body-dark");
+        $("a").removeClass("a-dark");
+    })
+
+
 })
+
+function changeTheme(){
+    var bodyColor = document.getElementsByTagName("body");
+    var heroImg = document.getElementById("main-img")
+
+    bodyColor.background == "#rgb(33,33,33)"
+
+    if (bodyColor = true){
+        
+    }
+}
