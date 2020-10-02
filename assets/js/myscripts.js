58babbcc0217e7127371df4fc1cd0f2203e3556f
inesnago <imsnago@gmail.com>
2020-10-02 19:04:23 +0000

    menu

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     $("#show-ul").mouseenter(function(){
         $(".sub-nav-ul").show("slow");
     });
+    
+    $("#show-ul").mouseleave(function(){
+        $(".sub-nav-ul").hide("slow");
+    });
 
     $('a').mouseenter(function(){
         $(this).addClass("shade-dark");
