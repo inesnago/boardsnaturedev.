ece99105e5ed7137dfa5513b0c243d6e5db62af7
inesnago <imsnago@gmail.com>
2020-10-06 14:57:26 +0000

    menu toggle almost finished / p size fixed / smaller screen response

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
-    $("#show-ul").mouseenter(function(){
-        $(".sub-nav-ul").show("slow");
-    });
 
-    $("#show-ul").mouseleave(function(){
-        $(".sub-nav-ul").hide("slow");
-    });
 
     $("a").mouseenter(function(){
         $(this).addClass("shade-dark");
@@ -16,8 +10,23 @@ $(document).ready(function(){
     });
 
     $(".logo-title").click(function(){
-        $(this).next(".small-card").fadeToggle("slow");
+        $(this).next(".small-card").fadeToggle("fast");
+    }); 
+    
+    $(".menu-modal").click(function(){
+        $(this).fadeOut("slow");
+            $(".main.nav").fadeOut("fast");
+    })
+
+    $(".toggle-icon").click(function(){
+        $(".menu-modal").fadeToggle("fast");
+    })
+
+    $("#show-ul").click(function(){
+        $(".sub-nav-ul").show("fast");
     });
+
+   
 
 })
 
